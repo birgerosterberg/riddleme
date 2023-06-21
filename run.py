7b725c456a28ecebabbc91a00dbd2d85a999ce47
Birger <birger.oesterberg@gmail.com>
2023-06-21 13:32:27 +0200

    add riddleme game start function

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def riddleme():
+    """
+    The main game function, that runs the riddleme game!
+    """
+    print("Welcome to the riddle game!\n First you need to tell me your name.\n Then after you have to tell me which ability you wish to learn. \n")
+    name = input("Enter your name: ")
+    ability = input("Enter your ability: ")
+    print(f"Hi there {name} i see you wish to have the ability to {ability}.")
+
+
+riddleme()
